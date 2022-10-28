643a7f9995189117ab6465049e0fb029cd354fc8
mektigboy <mektigboy@protonmail.com>
2022-10-28 13:01:17 +0800

    Update IDiamondCut.sol

---
+++ src/interfaces/IDiamondCut.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/// @title IDiamondCut
+/// @author mektigboy
+/// @author Modified from Nick Mudge: https://github.com/mudgen/diamond-3-hardhat
+/// @dev EIP-2535 "Diamond" standard
+interface IDiamondCut {
+    //////////////
+    /// EVENTS ///
+    //////////////
+
+    event DiamondCut(FacetCut[] cut, address init, bytes data);
+
+    ///////////////
+    /// STORAGE ///
+    ///////////////
+
+    /// ACTIONS
+
+    /// Add     - 0
+    /// Replace - 1
+    /// Remove  - 2
+
+    enum FacetCutAction {
+        Add,
+        Replace,
+        Remove
+    }
+
+    struct FacetCut {
+        address facetAddress;
+        FacetCutAction action;
+        bytes4[] functionSelectors;
+    }
+
+    /////////////
+    /// LOGIC ///
+    /////////////
+
+    function diamondCut(
+        FacetCut[] calldata cut,
+        address init,
+        bytes calldata data
+    ) external;
+}
