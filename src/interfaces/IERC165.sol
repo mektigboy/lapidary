6212b1634e5b1f09054cfcbf9a35819b09c98b0a
mektigboy <mektigboy@protonmail.com>
2022-10-28 13:02:07 +0800

    Update IERC165.sol

---
+++ src/interfaces/IERC165.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/// @title IERC165
+/// @author mektigboy
+interface IERC165 {
+    /////////////
+    /// LOGIC ///
+    /////////////
+
+    function supportsInterface(bytes4 id) external view returns (bool);
+}
