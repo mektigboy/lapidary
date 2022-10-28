e4df18bff27e97c482bb9c143edc5b78421ea830
mektigboy <mektigboy@protonmail.com>
2022-10-28 13:01:48 +0800

    Update IDiamondLoupe.sol

---
+++ src/interfaces/IDiamondLoupe.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/// @title IDiamondLoupe
+/// @author mektigboy
+/// @author Modified from Nick Mudge: https://github.com/mudgen/diamond-3-hardhat
+/// @dev EIP-2535 "Diamond" standard
+interface IDiamondLoupe {
+    ///////////////
+    /// STORAGE ///
+    ///////////////
+
+    struct Facet {
+        address facetAddress;
+        bytes4[] functionSelectors;
+    }
+
+    /////////////
+    /// LOGIC ///
+    /////////////
+
+    function facetAddress(bytes4 selector)
+        external
+        view
+        returns (address facetAddress);
+
+    function facetAddresses()
+        external
+        view
+        returns (address[] memory facetAddresses);
+
+    function facetFunctionSelectors(address facet)
+        external
+        view
+        returns (bytes4[] memory facetFunctionSelectors);
+
+    function facets() external view returns (Facet[] memory facets);
+}
