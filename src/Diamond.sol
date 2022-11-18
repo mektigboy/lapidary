add7d4db01f8d51efe876d8bded1f8344d91ee51
mektigboy <mektigboy@protonmail.com>
2022-11-18 13:27:44 +0800

    Update Diamond.sol

---
+++ src/Diamond.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./interfaces/IDiamondCut.sol";
+import "./libraries/LDiamond.sol";
+
+error Diamond__InvalidFunction();
+
+/// @title Diamond
+/// @author mektigboy
+/// @author Modified from Nick Mudge: https://github.com/mudgen/diamond-3-hardhat
+/// @notice Main contract of the diamond
+/// @dev Utilizes 'IDiamondCut', 'AppStorage' and 'LDiamond'
+contract Diamond {
+    /////////////
+    /// LOGIC ///
+    /////////////
+
+    constructor(address _owner, address _diamondCutFacet) payable {
+        LDiamond.updateContractOwner(_owner);
+
+        IDiamondCut.FacetCut[] memory cut = new IDiamondCut.FacetCut[](1);
+
+        bytes4[] memory functionSelectors = new bytes4[](1);
+
+        functionSelectors[0] = IDiamondCut.diamondCut.selector;
+        cut[0] = IDiamondCut.FacetCut({
+            facetAddress: _diamondCutFacet,
+            action: IDiamondCut.FacetCutAction.Add,
+            functionSelectors: functionSelectors
+        });
+
+        LDiamond.diamondCut(cut, address(0), "");
+    }
+
+    fallback() external payable {
+        LDiamond.DiamondStorage storage ds;
+
+        bytes32 position = LDiamond.DIAMOND_STORAGE_POSITION;
+
+        assembly {
+            ds.slot := position
+        }
+
+        address facet = ds.selectorToFacetAndPosition[msg.sig].facetAddress;
+
+        if (facet == address(0)) revert Diamond__InvalidFunction();
+
+        assembly {
+            calldatacopy(0, 0, calldatasize())
+
+            let result := delegatecall(gas(), facet, 0, calldatasize(), 0, 0)
+
+            returndatacopy(0, 0, returndatasize())
+
+            switch result
+            case 0 {
+                revert(0, returndatasize())
+            }
+            default {
+                return(0, returndatasize())
+            }
+        }
+    }
+}
